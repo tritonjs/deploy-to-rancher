717285829e4570c9a5529c1d55f7d0246115946d
Jared Allard <jaredallard@users.noreply.github.com>
2016-09-20 23:43:12 -0700

    rdeploy: clean up and use correct unzip syntax

--- rdeploy.sh
+++ rdeploy.sh
@@ -37,7 +37,7 @@ echo ""
 echo "-> Downloading Rancher Stack Configurations"
 PROJECT_CONFIG_URL=$RANCHER_URL"/environments/"$RANCHER_STACK_ID"/composeconfig"
 curl -L -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" $PROJECT_CONFIG_URL -o /tmp/config.zip
-unzip /tmp/config.zip -o/tmp/rc
+unzip /tmp/config.zip -d/tmp/rc
 rm config.zip
 
 # Remove stale AS & SK
@@ -51,4 +51,8 @@ pushd "/tmp/rc" >/dev/null
 echo ""
 echo "-> Updating service $RANCHER_SERVICE_NAME on $RANCHER_STACK_NAME"
 /tmp/rdeploy/rancher-compose -p $RANCHER_STACK_NAME up --force-upgrade --confirm-upgrade --pull -d $RANCHER_SERVICE_NAME
-popd"
+popd "" >/denull
+
+echo ""
+echo "-> Cleaning up"
+rm -rf /tmp/rc
