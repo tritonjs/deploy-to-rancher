44a8b6fd210e82f71cfcbe386dbb6bd253caf24b
Jared Allard <jaredallard@users.noreply.github.com>
2016-09-20 22:58:03 -0700

    rdeploy: base in /tmp

--- rdeploy.sh
+++ rdeploy.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+pushd "/tmp" >/dev/null
 
 echo ""
 echo "-> Downloading rancher-compose"
 curl -L \
 https://github.com/rancher/rancher-compose/releases/download/v0.9.2/rancher-compose-linux-amd64-v0.9.2.tar.gz \
--o rancher-compose.tar.gz \
+-o /tmp/rancher-compose.tar.gz
 tar zxvf rancher-compose.tar.gz --strip-components 2
-rm -rf rancher-compose.tar.gz \
-mv rancher-compose /usr/bin/rancher-compose \
-chmod +x /usr/bin/rancher-compose
+rm -rf rancher-compose.tar.gz
+
+mv rancher-compose /tmp/rdeploy/rancher-compose
+chmod +x /tmp/rdeploy/rancher-compose
 
 echo ""
 echo "-> Downloading Rancher Stack Configurations"
@@ -20,6 +22,6 @@ rm config.zip
 # Do Upgrade
 echo ""
 echo "-> Updating service $RANCHER_SERVICE_NAME on $RANCHER_STACK_NAME"
-rancher-compose -p $RANCHER_STACK_NAME up \
+/tmp/rdeploy/rancher-compose -p $RANCHER_STACK_NAME up \
     --force-upgrade --confirm-upgrade --pull \
 -d $RANCHER_SERVICE_NAME
